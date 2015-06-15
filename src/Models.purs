a328a0c6a346998797b26f997145e880b27a213f
Giacomo Tesio <giacomo@tesio.it>
2015-06-16 01:26:31 +0200

    better Ship

--- src/Models.purs
+++ src/Models.purs
@@ -12,7 +12,7 @@ data Point = Point { x :: Number
                    , y :: Number }
 
 instance pointShow :: Show Point where
-  show (Point {x=x, y=y}) = "{x: " ++ show x ++ ", y: " ++ show y ++ "}"
+  show (Point p) = "{x: " ++ show p.x ++ ", y: " ++ show p.y ++ "}"
 
 instance pointSemigroup :: Semigroup Point where
   (<>) (Point {x = a, y = b}) (Point {x = c, y = d}) = Point { x: (a + c), y: (b + d) }
@@ -25,12 +25,17 @@ data Ship = Ship { name :: String
                  , speed :: Point
                  , position :: Point }
 
-instance entityShip :: Entity Ship String where
-  identify (Ship { name = name }) = name
+instance shipEntity :: Entity Ship String where
+  identify (Ship s) = s.name
+
+instance shipShow :: Show Ship where
+  show (Ship s)= s.name ++ " {direction: " ++ show s.direction ++ ", speed: " ++ show s.speed ++ ", position: "++ show s.position ++" }"
 
 rotateShip :: Degree -> Ship -> Ship
-rotateShip n (Ship s@{direction = dir}) = Ship $ s { direction = (n + dir) % 360 }
+rotateShip n (Ship s) = Ship $ s { direction = (n + s.direction) % 360 }
 
 accelShip :: Number -> Ship -> Ship
-accelShip n (Ship s@{direction = dir, speed = speed}) = Ship $ s { speed = incr <> speed }
-                where incr = Point { x: n * (cos dir), y: n * (sin dir)}
+accelShip n (Ship s) = Ship $ s { speed = incr <> s.speed }
+                where incr = Point { x: n * dirCos, y: n * dirSin}
+                      dirSin = sin $ pi * (s.direction / 180)
+                      dirCos = cos $ pi * (s.direction / 180)
